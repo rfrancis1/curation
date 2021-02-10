4e387b5e7829b5d0ac6e09cac3a8097fb759413d
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2021-02-10 12:00:25 -0600

    Change parameters in the query for field checks from dataset to post_deid_dataset

--- data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier.py
@@ -1,20 +1,20 @@
+# %load_ext autoreload
+# %autoreload 2
+
+import pandas as pd
+
+project_id = "aou-res-curation-output-prod"
+dataset = "R2020Q4R2"
+
+from code.controlled_tier_qc import (run_all_checks, display_summary_checks, Table_Suppression_Check)
 
 
 
+# # Summary
 
+checks = run_all_checks(project_id, dataset)
 
-
-
-
-
-
-
-
-
-
-
-
-
+display_summary_checks(checks)
 
 
 
