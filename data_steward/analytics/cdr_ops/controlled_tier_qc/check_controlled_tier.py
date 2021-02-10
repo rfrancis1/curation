95f036fbebee9aae88c50fb15e3ba3e9863ce2e2
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2021-02-10 12:07:30 -0600

    Group result from summary checks by rule number

--- data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier.py
+++ data_steward/analytics/cdr_ops/controlled_tier_qc/check_controlled_tier.py
@@ -14,6 +14,10 @@ from code.controlled_tier_qc import (run_all_checks, display_summary_checks, Tab
 
 checks = run_all_checks(project_id, dataset)
 
+for rule, df in checks.items():
+    result = df.groupby('rule')['n_row_violation'].sum().reset_index()
+    print(result)
+
 display_summary_checks(checks)
 
 
