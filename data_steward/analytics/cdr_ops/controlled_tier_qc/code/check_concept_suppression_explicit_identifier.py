36e6157b8476fbf53745c6ce8b89924f5e76e5a3
ratsimfr <francis.r.ratsimbazafy.1@vumc.org>
2021-02-10 13:12:19 -0600

    Delete unused individual check code files

--- data_steward/analytics/cdr_ops/controlled_tier_qc/code/check_concept_suppression_explicit_identifier.py
+++
@@ -1,8 +0,0 @@
-# utils
-from controlled_tier_qc import Concept_Suppression_Check
-
-RULE_CODE = "DC-1364"
-
-def run_explicit_concept_suppression_check(project_id, dataset):
-    concept_qc = Concept_Suppression_Check(project_id, dataset, RULE_CODE)
-    return concept_qc._run_check()
--- data_steward/analytics/cdr_ops/controlled_tier_qc/code/check_concept_suppression_race_ethnicity.py
+++
@@ -1,8 +0,0 @@
-# utils
-from controlled_tier_qc import Concept_Suppression_Check
-
-RULE_CODE = "DC-1366"
-
-def run_race_concept_suppression_check(project_id, dataset):
-    concept_qc = Concept_Suppression_Check(project_id, dataset, RULE_CODE)
-    return concept_qc._run_check()
